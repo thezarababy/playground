e4ba8c2e912636ef4f15677b39c534c3972a9d5f
thezarababy <jyokoduwa@gmail.com>
2025-07-01 14:16:02 +0100

    usestate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Home = () => {
+
+const[count,setCount]= useState(9);
+
+const increaseCount=()=>{
+  setCount(count +1);
+};
+  const decreaseCount=()=>{
+    setCount(count -1);
+  };
+  const sqrt=()=>{
+    textl
+  }
+
   return (
     <div>
         This is the home page
+        <h1>your state value is:{count}</h1>
+        <button onClick={increaseCount}> Increase</button>
+        <button onClick={decreaseCount}>Decrease</button>
+        <button onClick={Math.sqrt(count)}>Square root</button>
     </div>
   )
 }
