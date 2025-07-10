cdb6c3643d8139c0040482a33e9246e4d8e92257
thezarababy <jyokoduwa@gmail.com>
2025-07-10 12:30:20 +0100

    form update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+  const[userDetails, setUserDetails]=useState({firstName:'',
+    lastName:'',
+     email:'',
+     password:'',
+     phoneNumber:'',
+  })
 
   const handleFormUpdate= (e) =>{
-    console.log(e.target.name, e.target.value, e.target.placeholder)
+
+   setUserDetails({...userDetails,
+    [e.target.name]: e.target.value,
+   })
   }
   return (
     <div>
@@ -11,19 +20,19 @@ const Contact = () => {
 
       
       <div className='border mb-6'>
-         First Name : <input type="text" name="firstName" value='' placeholder='enter your first name' onChange={handleFormUpdate}/> 
+         First Name : <input type="text" name="firstName" value={userDetails.firstName} placeholder='enter your first name' onChange={handleFormUpdate}/> 
       </div>
      <div className='border mb-6'>
-        last Name : <input type="text" name="lastName" value='' placeholder='enter your last name' onChange={handleFormUpdate}/>
+        last Name : <input type="text" name="lastName" value={userDetails.lastName} placeholder='enter your last name' onChange={handleFormUpdate}/>
      </div>
       <div className='border mb-6'>
-        email : <input type="email" name="email" value=''  placeholder='enter your email' onChange={handleFormUpdate}/>
+        email : <input type="email" name="email" value={userDetails.email}  placeholder='enter your email' onChange={handleFormUpdate}/>
       </div>
       <div className='border mb-6'>
-         password : <input type="password" name="password"  value='' placeholder='enter your password'onChange={handleFormUpdate}/>
+         password : <input type="password" name="password"  value={userDetails.password} placeholder='enter your password'onChange={handleFormUpdate}/>
       </div>
      <div className='border mb-6'>
-      phoneNumber : <input type="tel" name="phoneNumber" value='' placeholder='enter your phone number' onChange={handleFormUpdate} />
+      phoneNumber : <input type="tel" name="phoneNumber" value={userDetails.phoneNumber} placeholder='enter your phone number' onChange={handleFormUpdate} />
      </div>
      </form>
     </div>
