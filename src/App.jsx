3ddc1e113d5f297ccc524f932c4242c228cbade3
thezarababy <jyokoduwa@gmail.com>
2025-07-10 11:35:49 +0100

    form

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <div>
    
 <BrowserRouter>
- {/* <Header/> */}
+ { <Header/> }
 
 <Routes>
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
 const Contact = () => {
+
+  const handleFormUpdate= (e) =>{
+    console.log(e.target.name, e.target.value, e.target.placeholder)
+  }
   return (
-    <div>This is the contact page</div>
+    <div>
+      <form action="">
+
+      
+      <div className='border mb-6'>
+         First Name : <input type="text" name="firstName" value='' placeholder='enter your first name' onChange={handleFormUpdate}/> 
+      </div>
+     <div className='border mb-6'>
+        last Name : <input type="text" name="lastName" value='' placeholder='enter your last name' onChange={handleFormUpdate}/>
+     </div>
+      <div className='border mb-6'>
+        email : <input type="email" name="email" value=''  placeholder='enter your email' onChange={handleFormUpdate}/>
+      </div>
+      <div className='border mb-6'>
+         password : <input type="password" name="password"  value='' placeholder='enter your password'onChange={handleFormUpdate}/>
+      </div>
+     <div className='border mb-6'>
+      phoneNumber : <input type="tel" name="phoneNumber" value='' placeholder='enter your phone number' onChange={handleFormUpdate} />
+     </div>
+     </form>
+    </div>
   )
 }
 
